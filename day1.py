bda3bf5f5e79ef8a73452fd38b19249c534352b4
Gabriele Mongirdaite <gabriele.mongirdaite@vinted.com>
2024-11-30 14:11:48 +0000

    test day 1

---
+++ day1.py
@@ -0,0 +1,9 @@
+import time
+
+start_time = time.time()
+# print('1st part answer: ' + str(r))
+print("--- %s seconds for 1st part---" % (time.time() - start_time))
+
+start_time = time.time()
+# print('2nd part answer: ' + str(r))
+print("--- %s seconds for 2nd part---" % (time.time() - start_time))
