3718efffeff611b6153cca4bd03e98881ae3a8e0
gabrielemongirdaite <gabriele.mongirdaite@gmail.com>
2024-12-25 19:27:14 +0200

    advent of code day 24 part 2

--- day24.py
+++ day24.py
@@ -1,8 +1,5 @@
 import time
 from collections import deque
-import networkx as nx
-import matplotlib.pyplot as plt
-from itertools import combinations
 
 
 def read_file(file_name):
@@ -21,37 +18,6 @@ def read_file(file_name):
         w1, op, w2 = t.split(' ')
         gates.append((op, w1, w2, o))
     return wires, gates
-
-
-def add_nodes_to_graph(gates, wires):
-    G = nx.DiGraph()
-    for i in gates:
-        G.add_edge(i[1] + ' ' + i[0] + ' ' + i[2], i[3], weight=1)
-    for i in wires:
-        for j in gates:
-            if i in j[1] + ' ' + j[0] + ' ' + j[2]:
-                G.add_edge(i, j[1] + ' ' + j[0] + ' ' + j[2], weight=1)
-    return G
-
-
-def update_wires(gates_c, wires, z_wires):
-    mismatch = 0
-    step = 0
-    while gates_c and step <= 4000:
-        g = gates_c.popleft()
-        if g[1] in wires and g[2] in wires:
-            if g[0] == 'AND':
-                wires[g[3]] = 1 if wires[g[1]] == 1 and wires[g[2]] == 1 else 0
-            elif g[0] == 'OR':
-                wires[g[3]] = 1 if wires[g[1]] == 1 or wires[g[2]] == 1 else 0
-            else:
-                wires[g[3]] = wires[g[1]] ^ wires[g[2]]
-            if g[3][0] == 'z':
-                mismatch += abs(wires[g[3]] - z_wires[g[3]])
-        else:
-            gates_c.append(g)
-        step += 1
-    return wires, mismatch, step
 
 
 start_time = time.time()
@@ -91,6 +57,7 @@ start_time = time.time()
 
 result2 = 0
 wires, gates = read_file('input_day24.txt')
+
 result_x = ''
 for i in reversed(sorted(all_xs)):
     result_x += str(wires[i])
@@ -107,7 +74,50 @@ for ind, i in enumerate(correct_z[::-1]):
     else:
         z_wires['z' + str(ind)] = int(i)
 
+# by manually looking into input and considering how binary addition is done
+gates_c = gates.copy()
+wires_c = wires.copy()
 
+i = [('XOR', 'ccp', 'hhw', 'fph'), ('OR', 'snp', 'mnh', 'z15')]
+ind1 = gates_c.index(i[0])
+ind2 = gates_c.index(i[1])
+gates_c[ind1] = (i[0][0], i[0][1], i[0][2], i[1][3])
+gates_c[ind2] = (i[1][0], i[1][1], i[1][2], i[0][3])
 
-print('2nd part answer: ' + str(result2))
+i = [('XOR', 'nsp', 'tqh', 'gds'), ('AND', 'x21', 'y21', 'z21')]
+ind1 = gates_c.index(i[0])
+ind2 = gates_c.index(i[1])
+gates_c[ind1] = (i[0][0], i[0][1], i[0][2], i[1][3])
+gates_c[ind2] = (i[1][0], i[1][1], i[1][2], i[0][3])
+
+i = [('XOR', 'y30', 'x30', 'jrs'), ('AND', 'y30', 'x30', 'wrk')]
+ind1 = gates_c.index(i[0])
+ind2 = gates_c.index(i[1])
+gates_c[ind1] = (i[0][0], i[0][1], i[0][2], i[1][3])
+gates_c[ind2] = (i[1][0], i[1][1], i[1][2], i[0][3])
+
+i = [('XOR', 'ksm', 'fcv', 'cqk'), ('AND', 'ksm', 'fcv', 'z34')]
+ind1 = gates_c.index(i[0])
+ind2 = gates_c.index(i[1])
+gates_c[ind1] = (i[0][0], i[0][1], i[0][2], i[1][3])
+gates_c[ind2] = (i[1][0], i[1][1], i[1][2], i[0][3])
+
+while gates_c:
+    g = gates_c.popleft()
+    if g[1] in wires and g[2] in wires:
+        if g[0] == 'AND':
+            wires[g[3]] = 1 if wires[g[1]] == 1 and wires[g[2]] == 1 else 0
+        elif g[0] == 'OR':
+            wires[g[3]] = 1 if wires[g[1]] == 1 or wires[g[2]] == 1 else 0
+        else:
+            wires[g[3]] = wires[g[1]] ^ wires[g[2]]
+    else:
+        gates_c.append(g)
+
+for i in z_wires:
+    print(i, z_wires[i], wires[i])
+
+c = sorted(['fph', 'z15', 'z21', 'gds', 'wrk', 'jrs', 'cqk', 'z34'])
+
+print('2nd part answer: ' + str(','.join(c)))
 print("--- %s seconds for 2nd part---" % (time.time() - start_time))
